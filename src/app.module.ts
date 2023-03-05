0b79607fbdb9da3b03a512dc702e7f42e4599bf5
Fernando Brino <fernandobrino11@gmail.com>
2023-03-04 23:02:11 -0300

    Implemented typeorm and postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CostumersModule } from './costumers/costumers.module';
+import entities from './typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CostumersModule, UsersModule],
+  imports: [
+    CostumersModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nestjs_tutorial',
+      entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
