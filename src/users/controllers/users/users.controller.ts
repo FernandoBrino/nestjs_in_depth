fe2866190b5fd44f04244c06542eff12727f79aa
Fernando Brino <fernandobrino11@gmail.com>
2023-03-04 23:35:53 -0300

    Create user method

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,14 +1,21 @@
 import {
   Controller,
+  Body,
+  Param,
   Get,
+  Post,
   Inject,
   // NotFoundException, built in exception filter
   ParseIntPipe,
+  UseFilters,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
-import { Param, UseFilters, UseInterceptors } from '@nestjs/common/decorators';
 import { HttpStatus } from '@nestjs/common/enums';
 import { HttpException } from '@nestjs/common/exceptions';
 import { ClassSerializerInterceptor } from '@nestjs/common/serializer';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 import { UserNotFoundException } from 'src/users/exceptions/UserNotFound.exception';
 import { HttpExceptionFilter } from 'src/users/filters/HttpException.filter';
 import { UsersService } from 'src/users/services/users/users.service';
@@ -44,5 +51,11 @@ export class UsersController {
     if (user) return new SerializedUser(user);
     else throw new UserNotFoundException();
     // else throw new NotFoundException(); built in exception filter
+  }
+
+  @Post('create')
+  @UsePipes(ValidationPipe)
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
   }
 }
